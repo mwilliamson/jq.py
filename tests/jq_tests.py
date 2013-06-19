b015d280ab437989ebe568405d8894a283a29174
Michael Williamson <mike@zwobble.org>
2013-06-19 21:57:12 +0100

    Remove old TODO

--- tests/jq_tests.py
+++ tests/jq_tests.py
@@ -61,5 +61,4 @@ def output_elements_are_separated_by_newlines_when_there_are_multiple_inputs():
 
 @istest
 def value_error_is_raised_if_program_is_invalid():
-    # TODO: should raise in just jq()
     assert_raises(ValueError, lambda: jq("!"))
