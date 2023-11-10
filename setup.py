df59b40c984f9a0c15bee3ee308ced015e738e35
Michael Williamson <mike@zwobble.org>
2023-11-10 22:03:19 +0000

    Use git bash

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class jq_with_deps_build_ext(build_ext):
             tarball_path=jq_lib_tarball_path,
             lib_dir=jq_lib_dir,
             commands=[
-                ["bash", "./configure", "CFLAGS=-fPIC -pthread", "--disable-maintainer-mode", "--with-oniguruma=builtin"],
+                [r"C:\Program Files\Git\bin\bash.exe", "./configure", "CFLAGS=-fPIC -pthread", "--disable-maintainer-mode", "--with-oniguruma=builtin"],
                 ["cat", "Makefile"],
                 ["make", "V=1"],
             ])
