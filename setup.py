3fa4e4a462b57a480fb2036b1e1e599a2cad83b3
Michael Williamson <mike@zwobble.org>
2024-06-08 12:10:45 +0100

    Add Python 3.12 to tox and classifiers

--- setup.py
+++ setup.py
@@ -113,6 +113,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
 
