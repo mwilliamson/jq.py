f71ffb92da40ef24c429e576a58987b4e23a2ef8
Michael Williamson <mike@zwobble.org>
2025-12-18 16:00:55 +0000

    Bump version to 1.10.1

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ jq_extension = Extension(
 
 setup(
     name='jq',
-    version='1.10.0',
+    version='1.10.1',
     description='jq is a lightweight and flexible JSON processor.',
     long_description=_read("README.rst"),
     author='Michael Williamson',
