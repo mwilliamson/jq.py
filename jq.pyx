6e673584477544e0caf120636afe2bbbb92724ab
Michael Williamson <mike@zwobble.org>
2025-12-20 13:45:03 +0000

    Just a number

--- jq.pyx
+++ jq.pyx
@@ -1,8 +1,1 @@
-import io
-import json
-import threading
-
-from cpython.bytes cimport PyBytes_AsString
-from cpython.bytes cimport PyBytes_AsStringAndSize
-from libc.float cimport DBL_MAX
-from libc.math cimport INFINITY, modf
+x = 1
