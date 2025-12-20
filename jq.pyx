ea2df61b41d4c2e9211191ddb0d24c2c7f363232
Michael Williamson <mike@zwobble.org>
2025-12-20 13:44:53 +0000

    Just imports

--- jq.pyx
+++ jq.pyx
@@ -6,12 +6,3 @@ from cpython.bytes cimport PyBytes_AsString
 from cpython.bytes cimport PyBytes_AsStringAndSize
 from libc.float cimport DBL_MAX
 from libc.math cimport INFINITY, modf
-
-
-_compilation_lock = threading.Lock()
-
-
-class _EmptyValue(object):
-    pass
-
-_NO_VALUE = _EmptyValue()
