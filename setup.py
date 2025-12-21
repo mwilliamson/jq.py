567e39bb1d02f69914519974dcc6e0d38c00b6b0
Michael Williamson <mike@zwobble.org>
2025-12-21 19:38:55 +0000

    Remove licence classifier

--- setup.py
+++ setup.py
@@ -138,7 +138,6 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
